61e912904d487b794b1b8753816840fd83234249
David Guajardo Osorio <davidguajardoosorio@Mac-mini-de-David.local>
2018-05-11 17:58:44 -0300

    first commit

---
+++ testdg/testdg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testdg
+//
+//  Created by David Guajardo Osorio on 11-05-18.
+//  Copyright © 2018 David Guajardo Osorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
