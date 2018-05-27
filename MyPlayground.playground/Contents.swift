89c55d81b000486732f943076f70f5d1f408b9b2
David <david.guajardo.o@gmail.com>
2018-05-27 00:51:08 -0400

    Video 21 visto

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,8 +1,20 @@
 // Swift 4.0
-
 let myStr = """
             En un lugar de la mancha
             De cullo nombre no quiero acordarme
             """
-
 print(myStr)
+
+// String y colecciones
+let testStr = "Hola, soy David"
+print(testStr.reversed())
+
+for c in testStr {
+    print(c)
+}
+
+//print("String invertido")
+
+for c in testStr.reversed() {
+    print(c)
+}
