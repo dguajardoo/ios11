80f08d8b9ad39f6fa40c11b8f6910f50a97ef673
David <david.guajardo.o@gmail.com>
2018-05-27 23:23:01 -0400

    Video 35 visto

--- 002-Numeros.playground/Contents.swift
+++ 002-Numeros.playground/Contents.swift
@@ -13,3 +13,11 @@ var miEntero: Int = 6
 
 print("Valores de 32 bits: \(Int32.min), \(Int32.max)")
 print("Valores de 64 bits: \(Int64.min), \(Int64.max)")
+
+
+// Numeros Decimales
+let pi = 3.14159265 // Es un numero double
+let minT = -273.15 // Es un numero double
+
+let meaningOfLife = 42 // Es un numero entero, que vale 42
+let anotherPI = 3 + 0.14159265 // Es un double
