556596febc258cb48dc266178897bf9b70412269
David <david.guajardo.o@gmail.com>
2018-05-27 22:43:51 -0400

    Video 32 visto

---
+++ 001-Variables.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+// Variables: valores que van a cambiar
+// Constantes: valores de codigo que nunca cambian
+let maximunNumberOfLogin = 10
+var currentLoginAttempt = 0
+var x = 0.0, y = 0.0, z = 0.0
+
+//------------------------------------------------------
+
+var welcomeMessage:String
+welcomeMessage = "Hola"
+var red, green, blue: Double
+
+let π = 3.14159265
+let ☠️ = "muerte"
+
