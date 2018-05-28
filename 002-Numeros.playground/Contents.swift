d15d0e3a75027ec9fdaeae3be6e523a7fcd16540
David <david.guajardo.o@gmail.com>
2018-05-27 23:09:13 -0400

    Video 34 visto

---
+++ 002-Numeros.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Numeros
+// UInt8: entero sin signo de 8 bits
+// Int32: entero de 32 bits
+
+let minValue = UInt8.min // El valor minimo de un UInt8 es 0
+let maxValue = UInt8.max // El valor maximo de un UInt8 es 255
+
+var miEntero: Int = 6
+
+print("Valores de 32 bits: \(Int32.min), \(Int32.max)")
+print("Valores de 64 bits: \(Int64.min), \(Int64.max)")
