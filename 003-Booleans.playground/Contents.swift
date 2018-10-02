725e5dc89d171b5459bda7eff2cd13c27053cd4f
David Guajardo Osorio <davidguajardoosorio@Mac-mini-de-David.local>
2018-10-02 16:59:47 -0300

    Video 37 visto

---
+++ 003-Booleans.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Booleanos
+var myBool : Bool
+let orangeAreOrange = true
+let turnipAreDelicious = false;
+
+if turnipAreDelicious {
+    print("Buenos")
+} else {
+    print ("Malos")
+}
+
+let i = 3
+
+let myComparison = (i == 3)
+
+if i == 3 {
+    print("es un 3")
+}
