e9f10401c474eb7d3d7be9dd073cd68b92c04b2d
David Guajardo Osorio <davidguajardoosorio@Mac-mini-de-David.local>
2018-10-02 17:38:43 -0300

    Video 38 visto

--- 003-Booleans.playground/Contents.swift
+++ 003-Booleans.playground/Contents.swift
@@ -20,3 +20,31 @@ let myComparison = (i == 3)
 if i == 3 {
     print("es un 3")
 }
+
+
+// Tuplas
+let httpError : (Int, String) = (404, "Error, not found")
+let coordinates : (Int, Int, Int)
+let password : (String, Bool)
+
+let (statusCode, statusMessage) = httpError
+print("El código de error es \(statusCode) y el mensaje que devuelve es \(statusMessage)")
+
+let (justStatusCode, _) = httpError
+print("Me acaban de decir que el código de error es \(justStatusCode)")
+
+print("La primera parte es: \(httpError.0) y la segunda \(httpError.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+print(http200Status.statusCode)
+print(http200Status.description)
+
+
+
+
+
+
+
+
+
+
