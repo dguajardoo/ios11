5dc31379cdce44a250dc2a859c4867e4614ccf24
David Guajardo Osorio <davidguajardoosorio@Mac-mini-de-David.local>
2018-10-03 13:32:04 -0300

    Video 44 visto

---
+++ 02-001-Aritmerica.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Asignación
+let b = 12
+var a = 4
+print(a)
+a = b
+print(a);
+
+let (x, y) = (1, 5)
+print("x = \(x), y = \(y)")
+
+/*
+if (x = y) {
+    // Esto no es válido, ya que x = y no devuelve un valor, solamente lo asigna.
+}
+*/
+
+
+
+let minusSix = -6
+let alsoMinusSix = +minusSix // =minusSix, = +minusSix
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
